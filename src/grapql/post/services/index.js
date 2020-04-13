9ad92fa875574cde7b46155527f3d880b0c53d17
Saber Fathallah <saberfathallah7@gmail.com>
2020-04-13 13:17:00 +0200

    search query (#2)

--- src/grapql/post/services/index.js
+++ src/grapql/post/services/index.js
@@ -2,10 +2,12 @@ import addPostService from './addPostService';
 import getPostsByUserIdService from './getPostsByUserIdService';
 import getPostsByCategoryIdService from './getPostsByCategoryIdService';
 import getAllPostsService from './getAllPostsService';
+import searchService from './searchService';
 
 export {
   addPostService,
   getPostsByUserIdService,
   getPostsByCategoryIdService,
   getAllPostsService,
+  searchService,
 };
